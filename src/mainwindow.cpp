97b743b802156a9389ad843e0658665d02cde404
QiuLiang-99 <1297829693@qq.com>
2024-03-24 20:14:54 +0800

    设计ui中

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,27 +1,60 @@
 #include "mainwindow.h"
-#include <QLabel>
-#include <qboxlayout.h>
-#include <qframe.h>
+#include <qforeach.h>
+#include <qlist.h>
 #include <qpushbutton.h>
 
 MainWindow::MainWindow(QWidget* parent) : QWidget(parent) {
-  QGridLayout* gridLayout = new QGridLayout(this);
+  setupUI();
+}
 
-  gridLayout->setSpacing(0); // 表示各个控件之间的上下间距
+MainWindow::~MainWindow() {
+}
+
+void MainWindow::setupUI() {
+  this->setGeometry(0, 0, 800, 600);  // 规范窗体大小
+
+  gridLayout = new QGridLayout(this); // 主要布局
+  gridLayout->setSpacing(0);          // 表示各个控件之间的上下间距
+  QMargins blanket;
+  gridLayout->setContentsMargins(blanket);
+
 #if defined(Q_OS_WIN)
-
-  QVBoxLayout* buttonlayout = new QVBoxLayout;
+  // 创建布局放按钮
+  buttonlayout = new QVBoxLayout(this);
+  buttonlayout->setSizeConstraint(QLayout::SetFixedSize);
   buttonlayout->setSpacing(0);
+
+  mainWidget = new QStackedWidget(this);
+
   gridLayout->addLayout(buttonlayout, 0, 1);
-  QPushButton* pushbutton = new QPushButton;
-  buttonlayout->addWidget(pushbutton);
+  gridLayout->addWidget(mainWidget, 0, 2);
+  // 创建table按钮
+  QButtonGroup* tablebtnGroup = new QButtonGroup(this);
+  tablebtnGroup->setExclusive(true);
+  QList<QPushButton*> tablebtn = {new QPushButton("kaise"), new QPushButton,
+                                  new QPushButton, new QPushButton};
+  QSize               buttonSize(100, 25);
+  int                 index = 0;
+  for (auto pushbutton : tablebtn) {
+    pushbutton->setMinimumSize(buttonSize);
+    buttonlayout->addWidget(pushbutton);
+    tablebtnGroup->addButton(pushbutton, index++);
+    // pushbutton->setStyleSheet("pushbutton{margin:0}");
+  }
+  connect(tablebtnGroup, &QButtonGroup::idClicked, this, [&](int id) {
+    mainWidget->setCurrentIndex(id); // 用按钮对应的id切换stackedwidget的页面
+  });
+
   QLabel* l = new QLabel("Hello World"); // 新建Label
   buttonlayout->addWidget(l);
+
+  QWidget*            page_1       = new QWidget;
+  QHBoxLayout*        page_1H      = new QHBoxLayout(page_1);
+  CourseScheduleView* ScheduleView = new CourseScheduleView(page_1);
+  page_1H->addWidget(ScheduleView);
+  mainWidget->addWidget(page_1);
 
 #elif defined(Q_OS_ANDROID)
   QLabel* l = new QLabel("Hello Android");
 #endif
-}
-
-MainWindow::~MainWindow() {
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,11 +1,17 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "src/CourseScheduleView.h"
+#include <QLabel>
+#include <QStackedWidget>
 #include <QWidget>
+#include <qboxlayout.h>
+#include <qframe.h>
+#include <qpushbutton.h>
+#include <qsize.h>
+#include <qwidget.h>
 
-namespace Ui {
-class MainWindow;
-}
+#include <QButtonGroup>
 
 class MainWindow : public QWidget {
     Q_OBJECT
@@ -14,6 +20,11 @@ class MainWindow : public QWidget {
     explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+  public:
+    void            setupUI();
+    QGridLayout*    gridLayout;
+    QVBoxLayout*    buttonlayout;
+    QStackedWidget* mainWidget;
 };
 
 #endif // MAINWINDOW_H
